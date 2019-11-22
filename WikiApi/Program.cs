24e7183e59eab1df82a95bd404d6c4b0d581e535
James Potter <jamesrpotter1@gmail.com>
2019-11-22 21:25:58 +0000

    Initial commit

---
+++ WikiApi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace WikiApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string postgresConStr = "Server=localhost;Port=5433;UserId=postgres;Password=darkanima;Database=wikiapi;";
+            NpgsqlConnection dbConnection = new NpgsqlConnection(postgresConStr);
+            NpgsqlCommand cmd = new NpgsqlCommand(@"INSERT INTO wiki_page (id, name)
+                                                             VALUES (@id, 'first_wiki')", dbConnection);
+            cmd.Parameters.AddWithValue("id", NpgsqlDbType.Uuid, Guid.NewGuid());
+            dbConnection.Open();
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            var insertResult = dr.Read();
+            dbConnection.Close();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
