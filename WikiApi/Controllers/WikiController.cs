10e3adc1667a058f1fd0d70566b317b76a225255
James Potter <jamesrpotter1@gmail.com>
2019-11-22 23:54:31 +0000

    Add wiki controller

---
+++ WikiApi/Controllers/WikiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WikiApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WikiController : ControllerBase
+    {
+        private readonly IWikiStore _wikiStore;
+        
+        public WikiController(IWikiStore wikiStore)
+        {
+            _wikiStore = wikiStore ?? throw new ArgumentNullException(nameof(wikiStore));
+        }
+        
+        [HttpGet("name")]
+        public async Task<string> Index()
+        {
+            var wiki = await _wikiStore.GetWiki(Guid.Parse("e7437f88-98ab-4dfc-a7a8-d71644c601da"));
+            return (wiki != null) ? wiki.name : "Wiki not found";
+        }
+    }
+}
