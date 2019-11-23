a769ec3272f3f15e14ab429c9626765c89ed34d5
James Potter <jamesrpotter1@gmail.com>
2019-11-23 15:34:45 +0000

    Add get wiki button and input

--- jamwiki-web/src/views/Home.vue
+++ jamwiki-web/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <WikiHome msg="Welcome to Jam Wiki"/>
+    <b-button variant="primary">Get wiki</b-button>
+    <b-form-input v-model="wikiName" placeholder="Enter wiki name"></b-form-input>
   </div>
 </template>
 
