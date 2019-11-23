6ec2192660bfa0447dd90b5b834ed414c081bd49
James Potter <jamesrpotter1@gmail.com>
2019-11-23 13:12:18 +0000

    Add basis of vue frontend

---
+++ jamwiki-web/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div id="header">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#header {
+  display: flex;
+  justify-content: center;
+  background: #ebeaea;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #c8377a;
+}
+</style>
---
+++ jamwiki-web/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <WikiHome msg="This is an initial screen"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import WikiHome from '@/components/WikiHome.vue'
+
+export default {
+  name: 'home',
+  components: {
+    WikiHome
+  }
+}
+</script>
