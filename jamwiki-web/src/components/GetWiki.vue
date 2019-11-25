a1228093f569167743a4ac013611b00dd696fa9a
James Potter <jamesrpotter1@gmail.com>
2019-11-25 19:14:36 +0000

    Split get wiki into its own component

---
+++ jamwiki-web/src/components/GetWiki.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="wiki-input-wrapper">
+    <b-form-input id="wiki-input" v-model="wikiName" placeholder="Enter wiki name"></b-form-input>
+    <b-button id="get-wiki-but" variant="primary">Get wiki</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WikiHome',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#wiki-input-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1em;
+}
+
+#wiki-input {
+  display: flex;
+  margin-right: 1em; 
+  width: 11em;
+}
+
+#get-wiki-but {
+  width: 5.5em;
+}
+</style>
--- jamwiki-web/src/views/Home.vue
+++ jamwiki-web/src/views/Home.vue
@@ -1,42 +1,24 @@
 <template>
   <div class="home">
     <WikiHome msg="Welcome to Jam Wiki"/>
-    <div id="wiki-input-wrapper">
-      <b-form-input id="wiki-input" v-model="wikiName" placeholder="Enter wiki name"></b-form-input>
-      <b-button id="get-wiki-but" variant="primary">Get wiki</b-button>
-    </div>
+    <GetWiki/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import WikiHome from '@/components/WikiHome.vue'
+import GetWiki from '@/components/GetWiki.vue'
 
 export default {
   name: 'home',
   components: {
-    WikiHome
+    WikiHome,
+    GetWiki
   }
 }
 </script>
 
 <style scoped>
-
-#wiki-input-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1em;
-}
-
-#wiki-input {
-  display: flex;
-  margin-right: 1em; 
-  width: 11em;
-}
-
-#get-wiki-but {
-  width: 5.5em;
-}
 
 </style>
