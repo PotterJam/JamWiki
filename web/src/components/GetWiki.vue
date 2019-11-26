2f1561f97172b1cf167ba6478db9e82a1ddb1b73
James Potter <jamesrpotter1@gmail.com>
2019-11-26 21:42:55 +0000

    Get search working

--- web/src/components/GetWiki.vue
+++ web/src/components/GetWiki.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
     <div id="wiki-input-wrapper">
-      <b-form-input id="wiki-input" v-model="wikiName" placeholder="Enter wiki name..."></b-form-input>
-      <v-btn text icon color="primary" v-on:click="getWiki" id="wiki-but"><v-icon>mdi-alpha-b-circle</v-icon></v-btn>
+      <v-autocomplete
+        id="wiki-input"
+        v-model="wikiName"
+        autocomplete="off"
+        :items="wikiNames"
+        :loading="isLoading"
+        :search-input.sync="search"
+        hide-no-data
+        hide-selected
+        color="secondary"
+        item-text="Description"
+        item-value="API"
+        label="Wikis"
+        placeholder="Start typing to Search"
+        prepend-icon="mdi-magnify"
+        return-object
+      ></v-autocomplete>
       <v-btn text icon color="primary" v-on:click="addWiki" id="wiki-but"><v-icon>mdi-plus</v-icon></v-btn>
       <v-btn text icon color="primary" v-on:click="deleteWiki" id="wiki-but"><v-icon>mdi-delete</v-icon></v-btn>
     </div>
@@ -17,19 +32,14 @@ export default {
   name: 'GetWiki',
   data () {
     return {
-      wikiName: '',
-      wikiBody: ''
+      wikiName: null,
+      wikiBody: null,
+      wikiNames: [],
+      isLoading: false,
+      search: null
     }
   },
   methods: {
-    getWiki() {
-      this.axios
-          .get(`http://localhost:5000/api/wiki?name=${this.wikiName}`)
-          .then(response => {
-            this.wikiName = response.data.name;
-            this.wikiBody = response.data.body;
-          });
-    },
     addWiki() {
       this.axios
           .post('http://localhost:5000/api/wiki', {
@@ -44,6 +54,30 @@ export default {
             data: { name: this.wikiName }
           }).then(() => this.wikiBody = 'Wiki deleted')
     }
+  },
+  watch: {
+    wikiName(newWikiName) {
+      this.axios.get(`http://localhost:5000/api/wiki?name=${newWikiName}`)
+                .then(response => {
+                  this.wikiBody = response.data.body;
+                });
+    },
+    search () {
+      // Items have already been loaded
+      if (this.wikiNames.length > 0) return
+      // Items have already been requested
+      if (this.isLoading) return
+      this.isLoading = true
+      // Lazily load input items
+      this.axios
+        .get(`http://localhost:5000/api/wiki/names`)
+        .then(response => this.wikiNames = response.data)
+        .catch(err => {
+          // 
+          console.log(err)
+        })
+        .finally(() => (this.isLoading = false))
+    },
   }
 }
 </script>
@@ -59,10 +93,10 @@ export default {
 }
 
 #wiki-input-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: block;
+  margin: auto;
   padding-top: 1em;
+  width: 20em;
 }
 
 #wiki-input {
