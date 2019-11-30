0362920fc70e6461f7e1d1a8923e0e9764985026
James Potter <jamesrpotter1@gmail.com>
2019-11-30 13:28:17 +0000

    Remove about page

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Account from '../views/Account.vue'
 import store from '../store' // your vuex store
 
 Vue.use(VueRouter)
@@ -27,27 +24,19 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home,
+    component: () => import('../views/Home.vue'),
     beforeEnter: ifAuthenticated
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login,
+    component: () => import('../views/Login.vue'),
     beforeEnter: ifNotAuthenticated
   },
   {
     path: '/account',
     name: 'Account',
-    component: Account,
+    component: () => import('../views/Account.vue'),
     beforeEnter: ifAuthenticated
   }
 ]
