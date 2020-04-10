641289456856061772cb430ea45c2f403f3940a2
James Potter <jamesrpotter1@gmail.com>
2020-04-10 15:37:06 +0100

    Add productionbuild.sh

---
+++ productionBuild.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "CDing into web dir"
+cd web/  || { echo "CD into web dir failed" ; exit 1; }
+
+echo "Running npm run build"
+npm run build  || { echo "Npm run build failed" ; exit 1; }
+
+echo "Deleting old frontend version"
+rm -r /var/www/jampot.dev/html/*  || { echo "Deleting old frontend version failed" ; exit 1; }
+
+echo "Copying dist to /var/www/jampot.dev/html"
+cp -r web/dist/* /var/www/jampot.dev/html  || { echo "Copying failed" ; exit 1; }
+
+echo "Reloading nginx"
+systemctl reload nginx || { echo "Reloading nginx failed" ; exit 1; }
+
+echo "Running production docker build"
+docker run -d -p 5000:80 --env-file env.production --name wikiapi jamwiki || { echo "Docker run failed" ; exit 1; }
+
+echo "Production build complete and running :)"
